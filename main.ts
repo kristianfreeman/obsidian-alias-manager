0682ae46d771ca205268ff0200d896dca5ea9e88
Kristian Freeman <kristian@kristianfreeman.com>
2021-08-10 10:01:49 -0500

    Update with functionality

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, parseFrontMatterAliases, Vault, TFile } from 'obsidian';
+import mdify from 'mdify'
 
-interface MyPluginSettings {
-	mySetting: string;
+interface OAMSettings {
+	indexRegex: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: OAMSettings = {
+	indexRegex: String.raw`\d{1,}\.\d{1,}`
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class OAM extends Plugin {
+	settings: OAMSettings;
 
 	async onload() {
 		console.log('loading plugin');
@@ -20,41 +21,34 @@ export default class MyPlugin extends Plugin {
 			new Notice('This is a notice!');
 		});
 
-		this.addStatusBarItem().setText('Status Bar Text');
-
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
+			id: 'update-aliases',
+			name: 'Update Aliases',
+			callback: async () => {
+				this.updateAllFiles()
+			},
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new OAMSettingTab(this.app, this));
 
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
+			// console.log('codemirror', cm);
 		});
 
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			// console.log(this.app)
 		});
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+		const regex = new RegExp(this.settings.indexRegex)
+		this.app.vault.on("create", file => {
+			if (file instanceof TFile) {
+				this.updateFileAliases(this.app, file, regex)
+			} else {
+				console.log("File is TAbstractFile, not TFile, can't handle event")
+			}
+		})
 
-	onunload() {
-		console.log('unloading plugin');
+		await this.updateAllFiles()
 	}
 
 	async loadSettings() {
@@ -64,48 +58,66 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async updateAllFiles() {
+		const vault = this.app.vault
+		const files: [TFile] = (vault as any).fileMap
+		const regex = new RegExp(this.settings.indexRegex)
+		await Promise.all(Object.values(files).map((file: TFile) => this.updateFileAliases(this.app, file, regex)))
+	}
+
+	async updateFileAliases(app: App, file: TFile, regex: RegExp) {
+		const fileContent = await app.vault.read(file)
+
+		const nameWithoutIndex = file
+			.basename
+			.replace(regex, "")
+			.trim()
+
+		if (file.basename == nameWithoutIndex) return
+
+		const parsedFile = mdify.parse(fileContent)
+		let aliases: string[] = []
+
+		if (parsedFile.metadata && parsedFile.metadata.aliases) {
+			aliases = aliases.concat(parsedFile.metadata.aliases)
+
+			if (!parsedFile.metadata.aliases.includes(nameWithoutIndex)) {
+				aliases.push(nameWithoutIndex)
+			}
+		} else {
+			// Add frontmatter and aliases since there isn't any
+			aliases = [nameWithoutIndex]
+		}
+
+		const newContent = mdify.stringify({ aliases }, parsedFile.markdown)
+		await app.vault.modify(file, newContent)
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class OAMSettingTab extends PluginSettingTab {
+	plugin: OAM;
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: OAM) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Index Regex')
+			.setDesc("Index for parsing your index system. By default, this is the Johnny Decimal system, such as '12.34'.")
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder(DEFAULT_SETTINGS.indexRegex)
 				.setValue('')
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.indexRegex = value;
 					await this.plugin.saveSettings();
 				}));
 	}
